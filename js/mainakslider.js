9be4a23b6ee871acfa1e1fd34af8e6b13b09ca36
Joy <santunu23@gmail.com>
2020-04-12 12:15:59 +0600

    Update some sections.
    Update about me and image section.

--- js/mainakslider.js
+++ js/mainakslider.js
@@ -2,11 +2,17 @@
 	  var images=[];
 	  var time=3000;
 	  images[0]='img/patientimage/PreparedImage01.jpg';
-	  images[1]='img/patientimage/PreparedImage02.jpg';
-	  images[2]='img/patientimage/PreparedImage03.jpg';
-	  images[3]='img/patientimage/PreparedImage04.jpg';
-	  images[4]='img/patientimage/PreparedImage05.jpg';
-	  images[5]='img/patientimage/PreparedImage06.jpg';
+	  images[1]='img/patientimage/PreparedImage06.jpg';
+      images[2]='img/patientimage/Pictureone.jpg';
+	  images[3]='img/patientimage/Picturetwo.jpg';
+	  images[4]='img/patientimage/Picturethree.jpg';
+	  images[5]='img/patientimage/Picturefour.jpg';
+	  images[6]='img/patientimage/Picturefive.jpg';
+	  images[7]='img/patientimage/Picturesix.jpg';
+	  images[8]='img/patientimage/Pictureseven.jpg';
+	  images[9]='img/patientimage/Pictureeight.jpg';
+	  images[10]='img/patientimage/Picturenine.jpg';
+	
 	  
 	  
 	  function changeImg(){
